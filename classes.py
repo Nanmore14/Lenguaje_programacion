70260329ecee833f56e60e21f026309dcf6b239a
Nanmore14 <135552714+Nanmore14@users.noreply.github.com>
2024-02-17 10:12:02 -0600

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, id, name, major):
+    
+        self.name = name
+        self.najor = major 
+
+def __str__(self):
+    return f"Nombre: {self.name} /n carrera: {self.major}"
+    
+
+moreno = Student(1, "moreno", "ISI")
+print(moreno)
+print(moreno. __str_())
